ac756520ddb2f69868b4c280bf05e8a9edeff7a9
Fred <fredzyc@gmail.com>
2014-06-14 23:13:30 -0300

    cookie test

--- cookies.js
+++ cookies.js
@@ -11,8 +11,8 @@ function save(){
 		}else if(typeof stat[prop]==="string"){
 			console.log("SAVING prop:"+prop);
 			createCookie("stat."+prop,stat[prop]);	
-			console.log("SAVED stat."+prop+": "+getCookie("stat."+prop,stat[prop]));
-			console.log("SAVED stat."+prop+"typeof: "+typeof getCookie("stat."+prop,stat[prop]));
+			console.log("SAVED stat."+prop+": "+readCookie("stat."+prop,stat[prop]));
+			console.log("SAVED stat."+prop+"typeof: "+typeof readCookie("stat."+prop,stat[prop]));
 		}else if(typeof stat[prop]==="number"){
 			
 		}
@@ -21,34 +21,30 @@ function save(){
 	
 }
 
-//http://www.sitepoint.com/how-to-deal-with-cookies-in-javascript/
-function createCookie(name, value, expires, path, domain) {
-	var cookie = name + "=" + escape(value) + ";";
-  	if (expires) {
-    // If it's a date
-		if(expires instanceof Date) {
-			// If it isn't a valid date
-			if (isNaN(expires.getTime()))
-				expires = new Date();
-		}else
-			expires = new Date(new Date().getTime() + parseInt(expires) * 1000 * 60 * 60 * 24);
-	}else
-		expires = new Date(new Date().getTime() + 100 * 1000 * 60 * 60 * 24);
-	cookie += "expires=" + expires.toGMTString() + ";";
- 	if (path)
-    	cookie += "path=" + path + ";";
-  	if (domain)
-    	cookie += "domain=" + domain + ";";
- 
-  	document.cookie = cookie;
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}else{ 
+		var date = new Date();
+		date.setTime(date.getTime()+(100*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	document.cookie = name+"="+value+expires+"; path=/";
 }
-function getCookie(name) {
-  var regexp = new RegExp("(?:^" + name + "|;\s*"+ name + ")=(.*?)(?:;|$)", "g");
-  var result = regexp.exec(document.cookie);
-  return (result === null) ? null : result[1];
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
 }
-function deleteCookie(name, path, domain) {
-  // If the cookie exists
-  if (getCookie(name))
-    createCookie(name, "", -1, path, domain);
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
 }
